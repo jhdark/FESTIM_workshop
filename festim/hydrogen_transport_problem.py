4cb07db90c69d46afb3cd4ab98550d649ff53fed
J Dark <james.dark.95@gmail.com>
2023-11-14 10:38:40 -0500

    list of surface and volume subdomains

--- festim/hydrogen_transport_problem.py
+++ festim/hydrogen_transport_problem.py
@@ -112,6 +112,7 @@ class HydrogenTransportProblem:
         self.volume_meshtags = None
         self.formulation = None
         self.volume_subdomains = []
+        self.surface_subdomains = []
         self.bc_forms = []
         self.temperature_fenics = None
 
@@ -400,15 +401,19 @@ class HydrogenTransportProblem:
             tags_volumes = np.full(num_cells, 0, dtype=np.int32)
 
             for sub_dom in self.subdomains:
+                if isinstance(sub_dom, F.SurfaceSubdomain):
+                    self.surface_subdomains.append(sub_dom)
+                elif isinstance(sub_dom, F.VolumeSubdomain):
+                    self.volume_subdomains.append(sub_dom)
+
                 if isinstance(sub_dom, F.SurfaceSubdomain1D):
                     facet_index = sub_dom.locate_boundary_facet_indices(
                         self.mesh.mesh, self.mesh.fdim
                     )
                     facet_indices.append(facet_index)
                     tags_facets.append(sub_dom.id)
-                if isinstance(sub_dom, F.VolumeSubdomain1D):
+                elif isinstance(sub_dom, F.VolumeSubdomain1D):
                     # find all cells in subdomain and mark them as sub_dom.id
-                    self.volume_subdomains.append(sub_dom)
                     entities = sub_dom.locate_subdomain_entities(
                         self.mesh.mesh, self.mesh.vdim
                     )
@@ -429,12 +434,12 @@ class HydrogenTransportProblem:
             self.volume_meshtags = meshtags(
                 self.mesh.mesh, self.mesh.vdim, mesh_cell_indices, tags_volumes
             )
-            
-         # check volume ids are unique
+
+        # check volume ids are unique
         vol_ids = [vol.id for vol in self.volume_subdomains]
         if len(vol_ids) != len(np.unique(vol_ids)):
             raise ValueError("Volume ids are not unique")
-            
+
         # define measures
         self.ds = ufl.Measure(
             "ds", domain=self.mesh.mesh, subdomain_data=self.facet_meshtags
